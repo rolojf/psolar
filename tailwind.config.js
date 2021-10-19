a48c682c287a2657d74248db79b7b8a291a4dba2
Rolando Flores <rolo@tutamail.com>
2021-10-19 01:56:12 -0500

    ReadMe actualizado y selección de clases con match().groups

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
    purge: {
       enabled: true,
       content: ["./src/**/*.elm"],
-     defaultExtractor: (content) => { mmatchado = content.match(/class\s+".+"/g) || [];
+     defaultExtractor: (content) => { mmatchado = content.match(/class\s+"(.+)"/).groups || [];
                                       return mmatchado
                                     }
    },
